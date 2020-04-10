589a752db4e631143ecb4aa8b0c19c98f1e6a27b
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-04-10 18:13:04 +0530

    Execute suspending functions asynchronously with async-await.

--- src/main/kotlin/Async.kt
+++ src/main/kotlin/Async.kt
@@ -1,3 +1,4 @@
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlin.system.measureTimeMillis
@@ -5,9 +6,9 @@ import kotlin.system.measureTimeMillis
 fun main() = runBlocking {
     val time = measureTimeMillis {
         // These functions are executed sequentially and execute in 2000 ms, if we don't use async-await.
-        val one = doSomethingUsefulOne()
-        val two = doSomethingUsefulTwo()
-        println("result: ${one + two}")
+        val one = async { doSomethingUsefulOne() }
+        val two = async { doSomethingUsefulTwo() }
+        println("result: ${one.await() + two.await()}")
     }
     println("Time taken: $time")
 }
