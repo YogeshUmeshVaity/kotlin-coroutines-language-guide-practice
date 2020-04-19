3f9a5b49b568f5c46cc7abccb2c0ce20fc440b06
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-04-19 21:07:20 +0530

    Use flowOn() to specify the context in which the body of the flow{} will be called independent of the context in which the flow is collected.

--- src/main/kotlin/FlowOn.kt
+++ src/main/kotlin/FlowOn.kt
@@ -1,26 +1,31 @@
+package abc.anydiff123.flowOn
+
+import log
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 
-fun log(message: String) = println("${Thread.currentThread().name} : $message")
 
-fun fooFlow() = flow {
+
+@ExperimentalCoroutinesApi
+fun fooFlowOn() = flow {
     log("starting flow")
     for ( i in 1..3) {
+        log("Emitting $i")
         emit(i)
     }
-}
+}.flowOn(Dispatchers.Default)
 
 /**
  * Since fooFlow().collect() is called from worker thread, the body of the flow is also called
  * in the worker thread.
  */
+@ExperimentalCoroutinesApi
 fun main() = runBlocking {
     log("Before changing the context.")
-    withContext(Dispatchers.Default) {
-        fooFlow().collect { log("Collected $it") }
-    }
+    fooFlowOn().collect { log("Collected $it") }
 }
-
