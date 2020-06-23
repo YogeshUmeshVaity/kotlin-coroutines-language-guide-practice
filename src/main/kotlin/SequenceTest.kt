13b961a1c54b87e3e0a2a22e9e20be82d63605bc
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-06-23 22:10:05 +0530

    Use Sequence<T> to create generator function like in Javascript.

---
+++ src/main/kotlin/SequenceTest.kt
@@ -0,0 +1,29 @@
+package kotlinx.coroutines.guide.exampleFlow02
+
+/**
+ * The foo() function that we created here is like a generator function in Javascript.
+ * In Kotlin, generator functions are created using [Sequence] and yield().
+ * yield() is a suspending function. When the next() method is called on the Iterator object,
+ * the function that calls yield() function, returns the value passed to the yield() function
+ * and gets suspended at that point. It is resumed when then next() function is called on Iterator
+ * object again.
+ *
+ * So, think of the yield() function as "return, and next time start from where you stopped".
+ * This is how lazy iterators are created.
+ * These can also be used to create infinite elements unlike collections.
+ */
+fun foo(): Sequence<Int> = sequence { // sequence builder
+    for (i in 1..3) {
+        Thread.sleep(100) // pretend we are computing it
+        yield(i)          // yield next value
+    }
+}
+
+fun main() {
+    val iterator = foo().iterator()
+    println(iterator.next())    // 1
+    println(iterator.next())    // 2
+    println(iterator.next())    // 3
+
+    //foo().forEach { value -> println(value) }
+}
